fc621b66f222557d781cb5762bec49eead0468f2
Kimsae-ram <saeram0824@naver.com>
2025-10-11 14:04:26 +0900

    Add files via upload

--- 2451011.py
+++ 2451011.py
@@ -1,6 +1,87 @@
 ##print('안녕하세요', end="")
 ##print("안녕하세요", end='')
 
-print("\", \'는 따옴표의 대체 기호 입니다.")
-print("\\n은 줄바꿈의 대체 기호입니다.")
+##print("\", \'는 따옴표의 대체 기호 입니다.")
+##print("\\n은 줄바꿈의 대체 기호입니다.")
+
+##print("따옴표는 '역할'이 같아요.")
+##print('따옴표는 "역할"이 같아요.')
+
+##apple="사과"
+##print(apple,1,"상자")
+
+##apple='사과'
+##print(apple, 1, '상자', sep='++')
+
+##print("이샘")
+##print("코딩")
+##print("학원")
+
+##print("이샘",end="")
+##print("코딩",end="")
+##print("학원",end="!")
+
+##num1=10
+##print(num1, type(num1))
+
+##num2=3.14
+##print(num2,type(num2))
+
+##m_str="안녕하세요"
+##print(m_str, type(m_str))
+
+##print(1+1,type(1+1))
+
+##print(0.3+2.04, type(0.3+2.04))
+
+##print(1+2.3,type(1+2.3))
+
+##print("이샘코딩학원")
+
+##print(1,2,3,sep="+",end="=6")
+
+##print('"이샘코딩학원"')
+
+##num1=17
+##print(num1)
+##print("10진수 17=%d"%(num1))
+##print("10진수 17의 8진수=%o"%(num1))
+##print("10진수 17의 16진수=%x"%(num1))
+
+##num2=97
+##print("%d %c"%(num2, num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본출력 %f"%(float1))
+##print("지수표기법 %e"%(float1))
+##print("소수 둘째 자리까지 %.2f"%(float1))
+
+##txt1="hello"
+##txt2="world!"
+##print("기본 출력\n%s %s"%(txt1,txt2))
+##print("7칸 출력\n%7s %7s"%(txt1,txt2))
+
+##num1=17
+##print(num1)
+##print("10진수 17 ={0}".format(num1))
+##print("10진수 17의 8진수 ={0:o}".format(num1))
+##print("10진수 17의 16진수 ={0:x}".format(num1))
+
+##num2=98
+##print("{0} {1:c}".format(98,98))
+
+##float1=1234.567
+##print(float1)
+##print("기본 출력 {0:f}".format(float1))
+##print("지수 표기법 {0:e}".format(float1))
+##print("소수 둘째자리까지 {0:.2f}".format(float1))
+
+
+
+
+
+
+
+
 

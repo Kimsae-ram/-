b7a2b8728cdf4d98626cf4ddf385904c564b8222
Kimsae-ram <saeram0824@naver.com>
2025-10-18 17:50:11 +0900

    Add files via upload

---
+++ 251018.py
@@ -0,0 +1,186 @@
+##num1=17
+##print(num1)
+##print("10진수 17 = {0}" .format(num1))
+##print("10진수 17의 8진수 = {0:o}" .format(num1))
+##print("10진수 17의 16진수 = {0:x}" .format(num1))
+
+##num2=98
+##print("{0} {1:c}". format(num2, num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본 출력 {0:f}" .format(float1))
+##print("지수 표기법 {0:e}" .format(float1))
+##print("소수 둘째자리까지 {0:.2f}" .format(float1))
+
+####txt1="hello"
+####txt2="world!"
+####print("기본 출력 \n {0:s} {1:s}" .format(txt1, txt2))
+####print("7칸 출력 \n {0:7s} {1:7s}?" .format(txt1,txt2))
+##    
+##num1=17
+##print(num1)
+##print(f'10진수 17= {num1}')
+##print(f'10진수 17의 8진수 = {num1:o}')
+##print(f'10진수 17의 16진수 = {num1:x}')
+
+##txt1="hello"
+##txt2="world!"
+##print(f'기본 출력 \n {txt1:s} {txt2:s}')
+##print(f'7칸 츌력 \n {txt1:7s} {txt2:7s}?')
+
+##r=5
+##pi=3.14
+##print('넓이는 %d * %d * %.2f = %.1f' %(r,r,pi,pi*r*r))
+
+##r=5
+##pi=3.14
+##print('넓이는 {0:d} * {0:d} * {1:.2f} = {2:.1f}' .format(r,pi,r*r*pi))
+
+##r=5
+##pi=3.14
+##print(f'넓이는 {r:d} * {r:d} * {pi:.2f} = {r*r*pi:.1f}')
+
+
+##golden_ratio = 1.61804
+##print(f'소수 첫째 자리 까지 출력 : {golden_ratio:.1f}')
+##print(f'소수 둘째 자리 까지 출력 : {golden_ratio:.2f}')
+##print(f'소수 셋째 자리 까지 출력 : {golden_ratio:.3f}')
+##print(f'소수 넷째 자리 까지 출력 : {golden_ratio:.4f}')
+
+##egg = input("계란은 몇 개 ?")
+##print(egg, type(egg))
+
+##egg = input("계란은 몇 개 ?")
+###egg = int(egg)
+##print(egg, type(egg))
+
+##egg = input("계란은 몇 개?")
+##print(egg, type(egg))
+
+##egg = input("계란은 몇 개?")
+##egg = int(egg)
+##print(egg,type(egg))
+
+##attack = input('공격력은?')
+##scale = input('배율은?')
+##print('데미지는', attack*scale)
+
+##attack = int( input("공격력은?"))
+##scale = float( input("배율은?"))
+##print("데미지는", attack*scale)
+
+##minute = input('분을 입력하세요')
+##minute = int(minute)
+##second = 60*minute
+##text = str(minute)+'분='+str(second)+'초'
+##print(text)
+
+##nickname=input('닉네임을 입력하세요 :')
+##text = '환영합니다' + str(nickname) +'님!'
+##print(text)
+
+##num = float(input('실수를 입력하세요 :'))
+##result = num*10
+##print('{0:.3f}' .format(result))
+
+##first = int(input('첫번째 숫자를 입력하세요 :'))
+##second = int(input('두번째 숫자를 입력하세요 :'))
+##third = int(input('세번째 숫자를 입력하세요 :'))
+##print('총합은', first+second+third)
+
+##r = int(input('반지름을 입력하세요'))
+##print('원의 넓이는', r*r*3.14)
+##answer = 2* r* 3.14
+##print(f'원의 둘레길이는 {answer:.1f}')
+    
+##top = int(input('윗변을 입력하세요 :'))
+##bottom = int(input('아랫변을 입력하세요 :'))
+##height = int(input('높이를 입력하세요 :'))
+##answer = ((top+bottom)*height)/2
+##print(f'사다리꼴의 넓이는 {answer:.1f}')
+
+##nut1= '밤양갱'
+##nut2= '달디단 밤양갱'
+##nut3= """ 달디달고 달디달고,
+##달디단 밤양갱 밤양갱
+##내가 먹고 싶었던 건,
+##달디단, 밤양갱, 밤양갱이야"""
+##
+##sweet = '달디'
+##sweet2 = sweet*10
+##number = 255
+##message = sweet2 + '' + '단 밤양갱' + str(number) +'개'
+##print(message)
+
+##line = '='*20
+##header = '밤양갱 대박 세일'
+##footer = '놓치지 마세요!'
+##message = line + '\n' + header + '\n' + line +'\n' + footer + '\n' + line
+##print(message)
+
+ID = input('아이디 :')
+PW = input('비밀번호 :')
+str1 = 'ID'
+str2 = 'PW'
+message = '당신의 아이디는' +'"ID"' + '이며,' + '비밀번호는'+ '"PW"'+'입니다.'
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

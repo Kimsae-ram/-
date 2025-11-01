1344249c926cc10388d1ed72fedc1b83a9c16c44
Kimsae-ram <saeram0824@naver.com>
2025-11-01 18:00:35 +0900

    Add files via upload

---
+++ 251101.py
@@ -0,0 +1,239 @@
+Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+23
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+파이썬 10번 복습하기
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+문자열도    10.00번 복습하기
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+22
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 14, in <module>
+    print(len(number))
+TypeError: object of type 'int' has no len()
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+태어난 해를 입력해주세요: 1999
+태어난 월을 입력해주세요: 8
+태어난 일을 입력해주세요: 24
+당신의 생일은1999년 8월 24일입니다.HAPPY BIRTHDAY
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+10/20
+[10.1/20.2]
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+[ 3.1/0000000003]
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+[python    /python0000]
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+영화 제목을 입력하세요: 스파르타 리턴즈
+개봉 연도를 입력하세요: 2011
+주연 배우를 입력하세요: 이샘
+스파르타 리턴즈는 2011년에 개봉한 이샘 주연의 영화입니다.
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+아이디를 입력해주세요(6글자 이상) :leesam001
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 42, in <module>
+    secured_id=f'{id2:*8s}'
+ValueError: Invalid format specifier '*8s' for object of type 'str'
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+아이디를 입력해주세요(6글자 이상) :leesam001
+lees*****
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+아이디를 입력해주세요(6글자 이상) :leesam001
+lee*****
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+첫 번째 숫자 입력 : 22
+두 번째 숫자 입력 : 13
+num1 + num2 = 35
+num1 - num2 = 9
+num1 * num2 = 286
+num1 / num2 = 1.69
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+첫 번째 숫자 입력 : 22
+두 번째 숫자 입력 : 13
+num1 + num2 = 2213
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 54, in <module>
+    print(f'num1 - num2 = {num1 - num2}')
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자를 입력해주세요: 123
+173
+73
+6150
+2.460
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 ({}+{})*{}/{} = (3+4)*5/2
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (3+4)*5/2 = 17.5
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (w1+w2)*h/2 = 17.5
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (w1+w2)*h/2 = 17.5
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 71, in <module>
+    print('사다리 꼴의 넓이는 ({}+{])*{}/2 = {(w1+w2)*h/2}'.format(w1,w2,h))
+ValueError: unexpected '{' in field name
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 71, in <module>
+    print('사다리 꼴의 넓이는 ({}+{])*{}/2 = {(w1+w2)*h/2}'.format(w1,w2,h))
+ValueError: unexpected '{' in field name
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 71, in <module>
+    print('사다리 꼴의 넓이는 ({}+{])*{}/2 = 17.5'.format(w1,w2,h))
+ValueError: unexpected '{' in field name
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (3+4)*5/2 = 17.5
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 71, in <module>
+    print('사다리 꼴의 넓이는 ({}+{})*{}/2 = {(w1+w2)*h/2}'.format(w1,w2,h))
+KeyError: '(w1+w2)*h/2'
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (3+4)*5/2 = 17.5
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+사다리 꼴의 넓이는 (3+4)*5/2 = 17.5
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+첫 번째 숫자 입력 : 11
+두 번째 숫자 입력 : 3
+num1//num2 = 3
+num1%num2 = 2
+num1**num2 = 1331
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자를 입력해주세요 :13
+3
+1
+28561
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+몸무게(kg)를입력하세요 : 60
+키(m)를 입력하세요: 1.63
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 86, in <module>
+    print(f'BMI 지수는 {weight/height**2:.2f}')
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+몸무게(kg)를입력하세요 : 60
+키(m)를 입력하세요: 1.63
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 85, in <module>
+    height=int(input('키(m)를 입력하세요: '))
+ValueError: invalid literal for int() with base 10: '1.63'
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+몸무게(kg)를입력하세요 : 60
+키(m)를 입력하세요: 1.63
+BMI 지수는 22.58
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+물건의 가격을 입력합니다. :210
+500원 1개
+100원 2개
+50원 1개
+10원 4개
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+다섯 자리 숫자를 입력합니다 :54321
+만의 자리 : 5
+천의 자리 : 4
+백의 자리 : 3
+십의 자리 : 2
+일의 자리 : 1
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+물건의 가격을 입력합니다. :210
+500원 1개
+100원 2개
+50원 1개
+10원 4개
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+첫 번째 숫자 입력 : 22
+두 번째 숫자 입력 : 34
+세 번째 숫자 입력 : 5
+10000 += num1 -> 10022
+10000 -= num2 -> 9966
+10000 *= num3 -> 50000
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+문자열 입력 : test
+숫자 입력 : 5
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 111, in <module>
+    txt += num
+TypeError: can only concatenate str (not "int") to str
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+문자열 입력 : test
+숫자 입력 : 5
+test5
+testtesttesttesttest
+
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자 3개를 입력해주세요: 5 9 2
+Traceback (most recent call last):
+  File "C:/student/김새람/251101.py", line 117, in <module>
+    number1=int(input('숫자 3개를 입력해주세요: '))
+ValueError: invalid literal for int() with base 10: '5 9 2'
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자 3개를 입력해주세요: 5 9 2
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자 3개를 입력해주세요: 5 9 2
+84
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자를 입력해 주세요9
+삐약삐약삐약삐약삐약삐약삐약삐약삐약
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+계란 개수 입력 : 172
+계란 5판, 나머지 22개
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+피자를 몇 등분 할까? : 4
+한 조각을 몇 등분 할까? :3
+이 조각은 피자 한 판의 8.33%
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+7
+5
+3
+1
+>>> 
+======================= RESTART: C:/student/김새람/251101.py ======================
+숫자 5개를 입력해주세요:11 2 31 101 7
+30.400

cb71cbd6bb0ac9d4f24491d8e3c4e0d8fc504dd1
Kimsae-ram <saeram0824@naver.com>
2025-11-28 14:35:21 +0900

    Add files via upload

---
+++ 251128 homework.py
@@ -0,0 +1,70 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+273
+
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+273
+52.273
+
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+<class 'float'>
+
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+<class 'float'>
+<class 'int'>
+
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+0
+<class 'int'>
+0.0
+<class 'float'>
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+5+7= 12
+5-7= -2
+5*7= 35
+5/7= 0.7142857142857143
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+1.5
+1
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+5 % 2 = 1
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+2**1= 2
+2**2= 4
+2**3= 8
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+안녕하세요하세요하세요
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+안녕하세요하세요하세요
+안녕하세요안녕하세요안녕하세요
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+안녕하세요하세요하세요
+안녕하세요안녕하세요안녕하세요
+안녕하세요하세요하세요
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+#기본적인 연산
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+15 + 4 = 19
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+15 + 4 = 19
+15 + 4 = 19
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+15 + 4 = 19
+15 + 4 = 19
+15/4= 3.75
+>>> 
+========== RESTART: C:/Users/301196/Desktop/python/251128 homework.py ==========
+4.0
